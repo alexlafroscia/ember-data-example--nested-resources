76dd1313528cf8159ea54d2608380f25f512892b
Alex LaFroscia <alex@lafroscia.com>
2020-09-30 17:24:12 -0400

    chore: add prettier to eslint

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
-import Application from 'ember-data-load-resources/app';
-import config from 'ember-data-load-resources/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import Application from "ember-data-load-resources/app";
+import config from "ember-data-load-resources/config/environment";
+import { setApplication } from "@ember/test-helpers";
+import { start } from "ember-qunit";
 
 setApplication(Application.create(config.APP));
 
